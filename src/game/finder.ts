6a3d80e176f868cd6a5cf9b1461210d04a819857
Mirkobil <mirkobil.mirpayziev@gmail.com>
2025-05-15 10:04:44 +0500

    base

--- src/game/finder.ts
+++ src/game/finder.ts
@@ -1,46 +1,48 @@
-import { IPoint } from "../interface/IPoint";
+import { IPoint } from '../interface/IPoint';
 
 export type combination = {
-    type: number,
-    line: number,
-    coords: IPoint[],
+  type: number;
+  line: number;
+  coords: IPoint[];
 };
 
-export class Finder
-{
-    protected matrix!: number[][];
-    protected lines:number[][] = [
-        [0,0,0],
-        [1,1,1],
-        [2,2,2],
-    ];
-    
-    public find(matrix: number[][]): combination[]
-    {
-        this.matrix = matrix;
-        const arr: combination[] = [];
-        this.lines.forEach((v, i) => {
-            const combi = this.tryFindLine({coord: v, index: i});
-            if(combi) arr.push(combi);
-        });
-        return arr;
-    }
+export class Finder {
+  protected matrix!: number[][];
+  protected lines: number[][] = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [2, 2, 2],
+  ];
 
-    public tryFindLine(line: {coord: number[], index: number}): combination | undefined
-    {
-        const type = this.matrix[0][line.coord[0]];
-        const coord: IPoint[] = [];
-        
-        line.coord.some((r, c) => {
-            const equal = type === this.matrix[c][r];
-            if(equal) coord.push({x: c, y: r});
-            return !equal;
-        });
-        
-        return coord.length === line.coord.length ? {
-            type: type,
-            line: line.index,
-            coords: coord
-        } : undefined;
-    }
+  public find(matrix: number[][]): combination[] {
+    this.matrix = matrix;
+    const arr: combination[] = [];
+    this.lines.forEach((v, i) => {
+      const combi = this.tryFindLine({ coord: v, index: i });
+      if (combi) arr.push(combi);
+    });
+    return arr;
+  }
+
+  public tryFindLine(line: {
+    coord: number[];
+    index: number;
+  }): combination | undefined {
+    const type = this.matrix[0][line.coord[0]];
+    const coord: IPoint[] = [];
+
+    line.coord.some((r, c) => {
+      const equal = type === this.matrix[c][r];
+      if (equal) coord.push({ x: c, y: r });
+      return !equal;
+    });
+
+    return coord.length === line.coord.length
+      ? {
+          type: type,
+          line: line.index,
+          coords: coord,
+        }
+      : undefined;
+  }
 }
